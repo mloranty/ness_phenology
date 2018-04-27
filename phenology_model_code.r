31c67a611759ce9e9d7fe7be8b2fe9532242db1f
kroppheather <kroppheather@gmail.com>
2018-04-27 14:14:59 -0400

    started simple model run

--- phenology_model_code.r
+++ phenology_model_code.r
@@ -3,20 +3,26 @@
 ##############################################
 
 model{
+	#likelihood
 	for(i in 1:Nobs){
-		ndvi[i]~dnorm(mu.ndvi[i],tau.ndvi)
+		ndvi[i] ~ dnorm(mu.ndvi[i],tau.ndvi[vegID[i]])
 		mu.ndvi[i] <- base[vegID[i]]+ (Amp[vegID[i]]/(Greening[i]*Browning[i]))
-		Greening[i] <- 1+exp(slopeG[vegID[i]]*(G1[vegID[i]]-doy[i]))
-		Browning[i] <- 1+exp(slopeB[vegID[i]]*(doy[i]-B1[vegID[i]])
+		Greening[i] <- 1+exp(slopeG[vegID[i]]*(G1[vegID[i]]-doyN[i]))
+		Browning[i] <- 1+exp(slopeB[vegID[i]]*(doyN[i]-B1[vegID[i]]))
+		ndvi.rep[i] ~ dnorm(mu.ndvi[i],tau.ndvi[vegID[i]]) 
 	}
-
-	for(i in 1:Npixels){
+	#priors
+	for(i in 1:Nveg){
 		base[i] ~ dunif(0,.6)
 		Amp[i] ~ dunif(0,1)
 		slopeG[i] ~ dunif(0,300)
 		slopeB[i] ~ dunif(0,300)
 		G1[i] ~ dunif(0,.5)
 		B1[i] ~ dunif(0.5,1)
+		tau.ndvi[i] <- pow(sig.ndvi[i],-2)
+		sig.ndvi[i] ~ dunif(0,1)
+		half.G[i] <- G1[i]/slopeG[i]
+		half.B[i] <- B1[i]/slopeB[i]
 	}
 
 }
--- phenology_model_script.r
+++ phenology_model_script.r
@@ -1,13 +1,84 @@
 ##############################################
 ##script for fitting curves and analysis    ##
 ##############################################
+library(plyr)
+library(rjags)
+library(coda)
+library(mcmcplots)
 
-###############read in data###################
+###############################################
+###############read in data####################
+###############################################
 #cubesat data
-datP <- read.csv("c:\\Users\\hkropp\\Documents\\GitHub\\ness_phenology\\planet_veg_class_sample.csv")
+datP <- read.csv("z:\\projects\\ness_phenology\\planet_veg_class_sample.csv")
+modDI <- "z:\\projects\\ness_phenology\\sample_model\\run1"
 
 
+###############################################
+###############set up data for run ############
+###############################################
+###now look at data plot to make sure this curve makes sense
+plot(datP$doy,datP$ndvi)
+min(datP$ndvi)
+abline(v=193)
+#day 193 does not look reliable, clouds? exclude for now
+
+datP1 <- datP[floor(datP$doy)!=193,]
+plot(datP1$doy,datP1$ndvi)
+
+
+#organize a table of IDs, just start with vegetation
+
+vegDF <- unique(data.frame(veg=datP$veg))
+
+vegDF$vegID <- seq(1,dim(vegDF)[1])
+
+#join back into the dataframe
+datP2 <- join(datP1,vegDF, by="veg", type="left")
+
+
+#normalize day timeframe
+doyMin <- 135
+doyMax <- 273
+
+datP2$doyN <- (datP2$doy-doyMin)/(doyMax-doyMin)
+
+plot(datP2$doyN, datP2$ndvi)
+
+
+###############################################
+###############set up model run    ############
+###############################################
+
+datalist <- list(Nobs=dim(datP2)[1], ndvi=datP2$ndvi, vegID=datP2$vegID,doyN=datP2$doyN,
+				Nveg=dim(vegDF)[1])
+
+parms <- c("ndvi.rep", "sig.ndvi","base","Amp","slopeG","slopeB","G1","B1", "half.G","half.B")
+
+samp.modI<-jags.model(file="c:\\Users\\hkropp\\Documents\\GitHub\\ness_phenology\\phenology_model_code.r",
+						data=datalist,
+						n.adapt=5000,
+						n.chains=3)
+
+samp.sample <- coda.samples(samp.modI,variable.names=parms,
+                       n.iter=60000, thin=30)	
+			   
+mcmcplot(samp.sample, parms=c("sig.ndvi","base","Amp","slopeG","slopeB","G1","B1"),
+			dir=paste0(modDI,"\\history"))	
+					   
+mod.out <- summary(samp.sample)
+
+write.table(mod.out$statistics,paste0(modDI,"\\comp_mod_stats.csv"),
+			sep=",",row.names=TRUE)
+write.table(mod.out$quantiles,paste0(modDI,"\\comp_mod_quant.csv"),
+			sep=",",row.names=TRUE)
+
+
+
+###############################################
 ##############look at curves###################
+###############################################
+
 #negative
 phenC1 <- function(amp,base,c1,slope1,doy){
 
@@ -39,12 +110,4 @@ points(xval,yval3, type="l",lwd=2, col="tomato3")
 abline(v=.4)
 abline(v=.6)
 
-
-###now look at data plot to make sure this curve makes sense
-plot(datP$doy,datP$ndvi)
-
-#steps
-
-#remove negative values and potentially cloudy day
-#normalize days
-
+###################################################
